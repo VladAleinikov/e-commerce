8ea9709d3c0b2458f870b48db2b55c46d876b91b
Vladik <55589606+VladAleinikov@users.noreply.github.com>
2024-08-12 12:01:03 +0300

    add theme toggle

--- admin/app/layout.tsx
+++ admin/app/layout.tsx
@@ -5,6 +5,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { ModalProvider } from "@/providers/modal-provider";
 import { ToastProvider } from "@/providers/toast-provider";
 import { EdgeStoreProvider } from "@/lib/edgestore";
+import { ThemeProvider } from "@/providers/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,9 +24,16 @@ export default function RootLayout({
       <EdgeStoreProvider>
         <html lang="en">
           <body className={inter.className}>
-            <ModalProvider />
-            <ToastProvider />
-            {children}
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange
+            >
+              <ModalProvider />
+              <ToastProvider />
+              {children}
+            </ThemeProvider>
           </body>
         </html>
       </EdgeStoreProvider>
--- admin/components/navbar.tsx
+++ admin/components/navbar.tsx
@@ -4,9 +4,10 @@ import { StoreSwitcher } from "./store-switcher";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import prismadb from "@/lib/prismadb";
+import { ThemeToggle } from "./theme-toggle";
 
 export const Navbar = async () => {
-  const { userId} = auth();
+  const { userId } = auth();
 
   if (!userId) {
     redirect("/sign-in");
@@ -15,16 +16,17 @@ export const Navbar = async () => {
   const stores = await prismadb.store.findMany({
     where: {
       userId,
-    }
+    },
   });
 
   return (
     <div className="border-b">
       <div className="flex items-center h-16 px-4">
-        <StoreSwitcher items={stores}/>
+        <StoreSwitcher items={stores} />
         <MainNav />
         <div className="ml-auto flex items-center space-x-4">
-          <UserButton afterSignOutUrl="/"/>
+          <ThemeToggle />
+          <UserButton afterSignOutUrl="/" />
         </div>
       </div>
     </div>
